449b9094543db20e738561c69fede52ad2e592b2
Jiangnanxiaosheng <55862395+Jiangnanxiaosheng@users.noreply.github.com>
2025-01-15 19:43:32 +0800

    Create Singleton.hpp

---
+++ Singleton.hpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+inline int CHOOSE = 0;
+
+#if CHOOSE
+template <typename T>
+class Singleton {
+public:
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+    static T& getInstance() {
+        static T instance;
+        return instance;
+    }
+
+protected:
+    Singleton() = default;
+    virtual ~Singleton() = default;
+};
+
+# else
+template <typename T>
+class Singleton {
+public:
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+   	static std::shared_ptr<T> getInstance() {
+        static std::once_flag flag;
+        std::call_once(flag, [&]() {
+           m_instance  = std::shared_ptr<T>(new T);
+        });
+        return m_instance;
+    }
+
+    void printAddress() {
+        std::cout << m_instance.get() << std::endl;
+    }
+
+protected:
+    Singleton() = default;
+    virtual ~Singleton() = default;
+
+    static std::shared_ptr<T> m_instance;
+};
+
+template <typename T>
+std::shared_ptr<T> Singleton<T>::m_instance = nullptr;
+
+#endif
